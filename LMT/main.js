3526169f219e4065babf76d11b9e7811552a463a
Aly Ahmed Aly <99189600+AlyAhmed48@users.noreply.github.com>
2022-02-09 08:20:29 +0200

    Delete main.js

--- LMT/main.js
+++
@@ -1,7 +0,0 @@
-let myInput = document.getElementById("input");
-myInput.onfocus = function () {
-  myInput.style.boxShadow = "5px 5px 2px #3585c7";
-}
-myInput.onblur = function () {
-  myInput.style.boxShadow = "";
-}
